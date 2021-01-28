c14a58d3379c5692568d90c9f7a4d7200dd0e93a
Ahmad Raza <ahmadrazashafi@gmail.com>
2021-01-28 17:35:11 +0500

    First react mini project , Netflix

---
+++ src/index.js
@@ -0,0 +1,133 @@
+// var React = require('react');   //old js
+import React from 'react';    //babel
+// var ReactDOM = require('react-dom');  //old js
+import ReactDOM from 'react-dom';  //  with babel
+import "./index.css"
+
+// ReactDOM.render("What to See" , "Where to Show" ,  "call back functions") ;
+ReactDOM.render(
+  <React.Fragment>
+    <h1>NetFlix</h1>
+    <p>Top five NetFlix Series</p>
+  </React.Fragment>
+
+  , document.getElementById("root"));
+ReactDOM.render(
+  <React.Fragment>
+    <h2>2017</h2>
+    <ol>
+      <li>MoneyHiest</li>
+      <li>Fifty shades of darker</li>
+      <li>Heart boreken</li>
+      <li>MoneyHiest</li>
+      <li>MoneyHiest</li>
+    </ol>
+    <a>Click Here</a>
+  </React.Fragment>
+
+  , document.getElementById("category1"));
+
+  ReactDOM.render(
+    <React.Fragment>
+      <h2>2016</h2>
+      <ol>
+        <li>MoneyHiest</li>
+        <li>Fifty shades of darker</li>
+        <li>Heart boreken</li>
+        <li>MoneyHiest</li>
+        <li>MoneyHiest</li>
+      </ol>
+      <a>Click Here</a>
+    </React.Fragment>
+  
+    , document.getElementById("category1"));
+
+    
+
+    ReactDOM.render(
+      <React.Fragment>
+        <h2>2017</h2>
+        <ol>
+          <li>Monefdfsdf</li>
+          <li>Fifty shades of darker</li>
+          <li>Heart boreken</li>
+          <li>MoneyHiest</li>
+          <li>MoneyHiest</li>
+        </ol>
+        <a>Click Here</a>
+      </React.Fragment>
+    
+      , document.getElementById("category2"));
+  
+      ReactDOM.render(
+        <React.Fragment>
+          <h2>2018</h2>
+          <ol>
+            <li>MoneyHiest</li>
+            <li>Fifty shades of darker</li>
+            <li>Heart boreken</li>
+            <li>MoneyHiest</li>
+            <li>MoneyHiest</li>
+          </ol>
+          <a>Click Here</a>
+        </React.Fragment>
+      
+        , document.getElementById("category3"));
+
+        ReactDOM.render(
+          <React.Fragment>
+            <h2>2019</h2>
+            <ol>
+              <li>MoneyHiest</li>
+              <li>Fifty shades of darker</li>
+              <li>Heart boreken</li>
+              <li>MoneyHiest</li>
+              <li>MoneyHiest</li>
+            </ol>
+            <a>Click Here</a>
+          </React.Fragment>
+        
+          , document.getElementById("category4"));
+
+          
+          ReactDOM.render(
+            <React.Fragment>
+              <h2>2020</h2>
+              <ol>
+                <li>MoneyHiest</li>
+                <li>Fifty shades of darker</li>
+                <li>Heart boreken</li>
+                <li>MoneyHiest</li>
+                <li>MoneyHiest</li>
+              </ol>
+              <a>Click Here</a>
+            </React.Fragment>
+          
+            , document.getElementById("category5"));
+            ReactDOM.render(
+              <React.Fragment>
+                <h2>2021</h2>
+                <ol>
+                  <li>MirzaPur</li>
+                  <li>Fifty shades of darker</li>
+                  <li>Heart boreken</li>
+                  <li>MoneyHiest</li>
+                  <li>MoneyHiest</li>
+                </ol>
+                <a>Click Here</a>
+              </React.Fragment>
+            
+              , document.getElementById("category6"));
+          
+        
+    
+
+
+
+
+
+
+
+
+
+
